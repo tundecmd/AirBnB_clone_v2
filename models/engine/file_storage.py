6f870f266c2a4a9dda2b7231ca3fc2bff8ccbf21
Adepoju Lateef <ademustexcel@gmail.com>
2023-03-21 12:46:02 +0100

    added task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return self.__objects
+        # return FileStorage.__objects
+        results = {}
+        for key, val in self.__objects.items():
+            if isinstance(val, cls):
+                results[key] = val
+        return results
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +31,18 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """ delete obj from __objects if it’s inside """
+        if obj is None:
+            return
+        # print('self.__obj =>', self.__objects)
+        # print('obj.id', obj.id)
+        obj_key = obj.to_dict()['__class__'] + '.' + obj.id
+        # print('obj class id', obj_key)
+        if obj_key in self.__objects.keys():
+            del self.__objects[obj_key]
+            self.save()
 
     def reload(self):
         """Loads storage dictionary from file"""
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -6,7 +6,7 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 
 -- Grant all privileges on hbnb_test_db to hbnb_test
-GRANT ALL PRIVILEGES ON hbnb_dev_test.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
 
 -- Grant SELECT privilege on performance_schema to hbnb_test
